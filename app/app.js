03a818b327a64deeb5a4445e8367d07cbbb25a4d
vincent <jalzuritro@gmail.com>
2016-10-05 09:10:12 +0200

    Connexion fonctionnelle (redirection vers home) Ajout dépendance ngResource dans bower

--- app/app.js
+++ app/app.js
@@ -1,9 +1,13 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
+var services=angular.module('services',[]);
+var controllers = angular.module('controllers', []);
 var oonozApp = angular.module('oonozApp', [
     'ngRoute',
-    'LoginCtrl'
+    'ngResource',
+    'services',
+    'controllers'
 ]);
 
 oonozApp.config(['$locationProvider', '$routeProvider', '$httpProvider', function ($locationProvider, $routeProvider) {
@@ -11,6 +15,10 @@ oonozApp.config(['$locationProvider', '$routeProvider', '$httpProvider', functio
         .when('/login', {
             templateUrl: 'components/login/loginView.html',
             controller: 'LoginCtrl'
+        })
+        .when('/home',{
+            templateUrl: 'components/home/homeView.html',
+            //controller: 'LoginCtrl'
         })
         .otherwise({redirectTo: '/login'});
 }]);
--- app/components/login/loginService.js
+++ app/components/login/loginService.js
@@ -1,8 +1,26 @@
-/**
- * Created by tostrowski on 29/09/2016.
- */
-var loginService = angular.module('loginService', []);
 
-loginService.factory('LogService', function() {
+services.factory('LoginService', ['$resource','$location',
+    function($resource){
+        return{
+            login:function(username,password) {
+                return $resource("http://localhost:8080/user/login", {}, {
+                    login: {
+                        method: 'GET', headers: {Authorization: "Basic " + btoa(username + ":" + password)},
+                        cache: false, isArray: false
+                    }
+                });
+            } /*,
 
-});
+           logout:function(){
+                return $resource("http://localhost:8080/user/logout", {}, {
+                    logout: {
+                        method: 'POST',
+                        cache: false,
+                        isArray: false
+                    }
+                });
+
+            }*/
+
+
+        }}]);
