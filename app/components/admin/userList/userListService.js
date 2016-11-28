e1b3b93265f7078d1567ef8e6496abae50ebef3d
FlorianeGb <fl.goubel@gmail.com>
2016-11-28 08:54:35 +0100

    no message

--- app/components/admin/userList/userListService.js
+++ app/components/admin/userList/userListService.js
@@ -2,7 +2,7 @@ services.factory('UserListService', ['$resource','$location',
     function($resource) {
         return {
             filteredSearch: function () {
-                return $resource("http://localhost:8080/user/filteredSearch", {}, {
+                return $resource("http://localhost:8080/admin/filteredSearch", {}, {
 
                     query: {
                         method: 'POST',
@@ -12,7 +12,7 @@ services.factory('UserListService', ['$resource','$location',
                 });
             },
             updatePlayer: function(){
-                return $resource("http://localhost:8080/user/updatePlayer", {}, {
+                return $resource("http://localhost:8080/admin/updatePlayer", {}, {
                     query: {
                         method: 'POST',
                         cache: false,
@@ -21,7 +21,7 @@ services.factory('UserListService', ['$resource','$location',
                 });
             },
             updateSupplier: function(){
-                return $resource("http://localhost:8080/user/updateSupplier", {}, {
+                return $resource("http://localhost:8080/admin/updateSupplier", {}, {
                     query: {
                         method: 'POST',
                         cache: false,
