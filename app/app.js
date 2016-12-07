047957ffead76cef086bede92fd256a7b8313c50
Drem <thach.jeremy13@gmail.com>
2016-12-07 17:12:21 +0100

    [feature/listingQCM] add httpProvider to get authentication user

--- app/app.js
+++ app/app.js
@@ -16,7 +16,8 @@ var oonozApp = angular.module('oonozApp', [
     'naif.base64'
 ]);
 
-oonozApp.config(['$locationProvider', '$routeProvider', '$httpProvider', function ($locationProvider, $routeProvider) {
+oonozApp.config(['$locationProvider', '$routeProvider', '$httpProvider', function ($locationProvider, $routeProvider,$httpProvider) {
+    $httpProvider.defaults.withCredentials = true;
     $routeProvider
         .when('/login', {
             templateUrl: 'components/login/loginView.html',
