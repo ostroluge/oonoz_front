8594b351a04633d0f18a95bec4c222e192ae0ca1
Drem <thach.jeremy13@gmail.com>
2017-02-25 19:29:07 +0100

    [feature/userAccount] - add user page - user can request for being supplier - new home design

--- app/components/profil/profilService.js
+++ app/components/profil/profilService.js
@@ -14,7 +14,39 @@ services.factory('profilService', ['$resource', '$location',
                         isArray: false
                     }
                 });
+            },
+            updatePlayer: function(){
+                return $resource("http://localhost:8092/admin/updatePlayer", {}, {
+                    // return $resource("http://localhost:8092/admin/updatePlayer", {}, {
+                    query: {
+                        method: 'PUT',
+                        cache: false,
+                        isArray: false
+                    }
+                });
+            },
+            updateSupplier: function(){
+                return $resource("http://localhost:8092/admin/updateSupplier", {}, {
+                    // return $resource("http://localhost:8092/admin/updateSupplier", {}, {
+                    query: {
+                        method: 'PUT',
+                        cache: false,
+                        isArray: false
+                    }
+                });
+            },
+            requestSupplierStatus: function(idPlayer){
+                console.log("service supplier Status :  "+idPlayer);
+                return $resource("http://localhost:8092/user/requestSupplierStatus", {}, {
+                    query: {
+                        method: 'GET',
+                        cache: false,
+                        isArray: false,
+                        params:{
+                            "idPlayer":idPlayer
+                        }
+                    }
+                });
             }
-
         }
     }]);
