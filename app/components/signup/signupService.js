e54ca496ca5a97e09d404ed141fd2c9dbf029984
jilief <flamen.julien@gmail.com>
2017-03-01 13:37:27 +0100

    Test modification endpoint prod

--- app/components/signup/signupService.js
+++ app/components/signup/signupService.js
@@ -4,8 +4,8 @@ services.factory('SignUpService', ['$resource','$location',
     function($resource){
         return {
             signUpPlayer: function () {
-              //  return $resource("http://localhost:8080/user/signUpPlayer", {}, {
-                    return $resource("http://localhost:8092/user/signUpPlayer", {}, {
+              //  return $resource("http://localhost:8092/user/signUpPlayer", {}, {
+                    return $resource("http://5.135.165.108:8092/user/signUpPlayer", {}, {
 
                     save: {
                         method: 'POST',
@@ -26,7 +26,7 @@ services.factory('SignUpService', ['$resource','$location',
             },
             validationSignUp: function (mail,key) {
                // return $resource("http://localhost:8080/user/validationMail?mail="+mail+"&key="+key, {}, {
-                    return $resource("http://localhost:8092/user/validationMail?mail="+mail+"&key="+key, {}, {
+                    return $resource("http://5.135.165.108:8092/user/validationMail?mail="+mail+"&key="+key, {}, {
 
                     query: {
                         method: 'GET',
